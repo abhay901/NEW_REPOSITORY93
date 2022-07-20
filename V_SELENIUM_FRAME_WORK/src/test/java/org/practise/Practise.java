0ad1f0a037ff44989cbafa05da96367fc9b6c6cf
admin dell <admin dell@DESKTOP-80HLJ8Q>
2022-07-20 18:17:04 +0530

    ggug

---
+++ V_SELENIUM_FRAME_WORK/src/test/java/org/practise/Practise.java
@@ -0,0 +1,23 @@
+package org.practise;
+
+import org.testng.annotations.Test;
+
+public class Practise 
+{
+	@Test
+	public void test()
+	{
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+		System.out.println("Test is Passed");
+	}
+
+}
